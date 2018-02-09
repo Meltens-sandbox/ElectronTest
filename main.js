80997235b5d9bd4935dd78f57e7b5cab1077eeee
aznalo <irimamekun@gmail.com>
2018-02-09 16:51:58 +0900

    first commit

--- main.js
+++ main.js
@@ -15,7 +15,9 @@ app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/src/html/index.html');
+  // mainWindow.loadURL('file:///home/assetmilk/.ghq/github.com/aznalo/ElectronTest/src/html/index.html');
+  console.log(__dirname);
 
   mainWindow.on('closed', function() {
     mainWindow = null;
